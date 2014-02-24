05fc1baaa118a4a51def689220f83990e4297dc3
mreiben <jason.eiben@gmail.com>
2014-02-23 20:51:12 -0500

    Repo added

---
+++ demo/app/controllers/say_controller.rb
@@ -0,0 +1,13 @@
+class SayController < ApplicationController
+  def hello
+    @time= Time.now
+  end
+
+  def goodbye
+  end
+  
+  def filenames
+    @files = Dir.glob('*')
+    return @files
+  end
+end
---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = '4d2de83bb468bf769dc75e6f8a2d6368b7165e2cb5dc2ec7bf697ab6a17e0b4a82798d909b45d1ac1bfd257f4e8b9f632ea09f6975fb3b266b8dfbd481fbc3bb'
